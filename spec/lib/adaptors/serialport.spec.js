a6bf4232abc8e844f8864e5c451eed5d85a4441b
Andrew Stewart <andrew@stwrt.ca>
2015-05-29 16:36:26 -0700

    Directly stub serialport

--- spec/lib/adaptors/serialport.spec.js
+++ spec/lib/adaptors/serialport.spec.js
@@ -1,12 +1,22 @@
 "use strict";
 
+var serialport = require("serialport");
+
+var EventEmitter = require("events").EventEmitter;
+
 var Adaptor = lib("adaptors/serialport");
 
 describe("Serialport Adaptor", function() {
-  var adaptor;
+  var adaptor, port;
 
   beforeEach(function() {
     adaptor = new Adaptor("/dev/rfcomm0");
+    port = new EventEmitter();
+    stub(serialport, "SerialPort").returns(port);
+  });
+
+  afterEach(function() {
+    serialport.SerialPort.restore();
   });
 
   it("is a class", function() {
@@ -16,7 +26,6 @@ describe("Serialport Adaptor", function() {
   describe("#constructor", function() {
     it("sets @conn", function() {
       expect(adaptor.conn).to.be.eql("/dev/rfcomm0");
-
     });
 
     it("sets @serialport", function() {
@@ -29,77 +38,86 @@ describe("Serialport Adaptor", function() {
   });
 
   describe("#open", function() {
-    var callback, serialport;
+    var callback;
 
     beforeEach(function() {
       callback = spy();
-
-      serialport = { on: stub() };
-      serialport.on.yields();
-
-      stub(adaptor, "emit");
-      stub(adaptor, "newSerialport");
-      adaptor.newSerialport.returns(serialport);
-
       adaptor.open(callback);
     });
 
-    afterEach(function() {
-      adaptor.newSerialport.restore();
-      adaptor.emit.restore();
+    it("creates a new SerialPort instance", function() {
+      expect(adaptor.serialport).to.be.eql(port);
     });
 
-    it("sets @serialport to an instance of SerialPort", function() {
-      expect(adaptor.newSerialport).to.be.calledOnce;
+    it("doesn't automatically trigger the callback", function() {
+      expect(callback).to.not.be.called;
     });
 
-    it("calls @serialport.on with open", function() {
-      expect(adaptor.serialport.on).to.be.calledWith("open");
-      expect(adaptor.emit).to.be.calledWith("open");
-    });
+    context("when the serialport emits 'open'", function() {
+      var emit;
 
-    it("calls @serialport.on with error", function() {
-      expect(adaptor.serialport.on).to.be.calledWith("error");
-      expect(adaptor.emit).to.be.calledWith("error");
-    });
+      beforeEach(function() {
+        emit = adaptor.emit = spy();
+      });
 
-    it("calls @serialport.on with data", function() {
-      expect(adaptor.serialport.on).to.be.calledWith("data");
-      expect(adaptor.emit).to.be.calledWith("close");
-    });
+      context("if there wasn't an error", function() {
+        beforeEach(function() {
+          port.emit("open");
+        });
 
-    it("calls @serialport.on with close", function() {
-      expect(adaptor.serialport.on).to.be.calledWith("data");
-      expect(adaptor.emit).to.be.calledWith("data");
-    });
+        it("triggers the callback", function() {
+          expect(callback).to.be.called;
+        });
 
-    it("triggers the callback", function() {
-      expect(callback).to.be.calledOnce;
-    });
+        it("emits 'open'", function() {
+          expect(emit).to.be.calledWith("open");
+        });
 
-    it("calls callback with error when one present", function() {
-      var err = new Error("Some Error!");
-      serialport.on.yields(err);
-      adaptor.open(callback);
-      expect(callback).to.be.calledWith(err);
+        it("passes through 'error' events", function() {
+          port.emit("error", "error message");
+          expect(emit).to.be.calledWith("error", "error message");
+        });
+
+        it("passes through 'close' events", function() {
+          port.emit("close", "port closed");
+          expect(emit).to.be.calledWith("close", "port closed");
+        });
+
+        it("passes through 'data' events", function() {
+          port.emit("data", "new data");
+          expect(emit).to.be.calledWith("data", "new data");
+        });
+      });
+
+      context("if there was an error while opening", function() {
+        beforeEach(function() {
+          port.emit("open", "error!");
+        });
+
+        it("triggers the callback with the error", function() {
+          expect(callback).to.be.calledWith("error!");
+        });
+
+        it("doesn't emit 'open'", function() {
+          expect(emit).to.not.be.calledWith("open");
+        });
+      });
     });
   });
 
   describe("function", function() {
-    var data, callback, serialport;
+    var data, callback;
 
     beforeEach(function() {
-      serialport = {
-        write: stub(),
-        close: stub()
-      };
+      port.write = stub();
+      port.close = stub();
 
       stub(adaptor, "on");
 
       data = [0x0f, 0x10, 0x03];
       callback = spy();
 
-      adaptor.serialport = serialport;
+      adaptor.serialport = port;
     });
 
     it("#write calls @serialport#write with", function() {
@@ -117,13 +135,5 @@ describe("Serialport Adaptor", function() {
       expect(adaptor.serialport.close).to.be.calledWith(callback);
     });
   });
-
-  // describe("#newSerialport", function() {
-  //   it("creates a new instance of SerialPort", function() {
-  //     var SerialPort = require("serialport").SerialPort;
-  //     var serialport = adaptor.newSerialport();
-  //     expect(serialport).to.be.an.instanceof(SerialPort);
-  //   });
-  // });
 });
 
